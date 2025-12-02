8f75db355983b5550f59f8ecce783fd1a803f544
Lucas-Blanger <blangerlucas@gmail.com>
2025-12-02 15:21:25 -0300

    edit problem 1261

--- C++/1261.cpp
+++ C++/1261.cpp
@@ -4,41 +4,34 @@
 using namespace std;
 
 int main(){
-   int m, n;
-   cin >> m >> n;
-   
-   map<string, long long> dic;
-   for(int i = 0; i < m; i++){
-      string cargo;
-      long long sal;
-      cin >> cargo >> sal;
-      dic[cargo] = sal;
-   }
-   
-   cin.ignore(); 
-   
-   for(int i = 0; i < n; i++){
-      string descricao = "";
-      
-      while(true){
-         string linha;
-         getline(cin, linha);
-         
-         if(linha == ".") break;
-         
-         descricao += linha + " "; 
+  int m, n;
+  cin >> m >> n;
+  map<string, int> cargos;
+
+
+  for(int i = 0; i < m; i++){
+      string nome;
+      int valor;
+      cin >> nome >> valor;
+      cargos[nome] = valor;
+  }
+
+  for(int i = 0; i < n; i++){
+      string linha, texto;
+      while(getline(cin, linha)){
+         if(linha[0] == '.') break;
+         texto += linha;
+         texto += " ";
       }
-      
-      stringstream ss(descricao);
-      string palavra;
-      long long total = 0;
-      
-      while(ss >> palavra){
-         total += dic[palavra]; 
+      int preco = 0;
+      stringstream ss(texto);
+      string aux;
+      while(ss >> aux){
+         preco += cargos[aux];
       }
-      
-      cout << total << endl;
-   }
+
+      cout << preco << endl;
+  }
    
 
 }
