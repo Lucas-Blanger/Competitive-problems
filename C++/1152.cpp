9fb01cc7fa3a7a8273f8c94f0a694db227f837e5
Lucas-Blanger <blangerlucas@gmail.com>
2026-05-06 10:10:02 -0300

    add problem 1152

---
+++ C++/1152.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+
+
+using namespace std;
+
+struct DSU {
+    vector<int> pai, rank_;
+ 
+    DSU(int n) : pai(n), rank_(n, 0) {
+        iota(pai.begin(), pai.end(), 0);
+    }
+ 
+    int find(int x) {
+        if (pai[x] != x) pai[x] = find(pai[x]);
+        return pai[x];
+    }
+ 
+    bool unite(int a, int b) {
+        a = find(a); b = find(b);
+        if (a == b) return false;
+        if (rank_[a] < rank_[b]) swap(a, b);
+        pai[b] = a;
+        if (rank_[a] == rank_[b]) rank_[a]++;
+        return true;
+    }
+};
+
+long long kruskal(int n, vector<tuple<int,int,int>> edges) {
+    sort(edges.begin(), edges.end());
+ 
+    DSU dsu(n);
+    long long custo = 0;
+    int arestas = 0;
+ 
+    for (auto& [w, u, v] : edges) {
+        if (dsu.unite(u, v)) {
+            custo += w;
+            if (++arestas == n - 1) break;
+        }
+    }
+ 
+    return (arestas == n - 1) ? custo : -1;
+}
+
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int r,c;
+    while(true){
+        cin >> r >> c;
+        if(r == 0 && c == 0) break;
+
+        vector<tuple<int,int,int>> edges; // {peso, u, v}
+
+        long long total = 0;
+        for(int i = 0; i < c; i++){
+            int v,w,p;
+            cin >> v >> w >> p;
+            edges.push_back({p,v,w});
+            total += p;
+        }
+
+
+        long long resultado = kruskal(r, edges);
+        cout << (total - resultado) << "\n";
+    }
+    
+ 
+
+
+}
--- README.md
+++ README.md
@@ -468,3 +468,4 @@ The repository is organized as follows:
 |                      [1077](https://www.beecrowd.com.br/repository/UOJ_1077.html)                      |     Beecrowd      |    C++     |
 |                      [2566](https://www.beecrowd.com.br/repository/UOJ_2566.html)                      |     Beecrowd      |    C++     |
 |                      [1774](https://www.beecrowd.com.br/repository/UOJ_1774.html)                      |     Beecrowd      |    C++     |
+|                      [1152](https://www.beecrowd.com.br/repository/UOJ_1152.html)                      |     Beecrowd      |    C++     |
